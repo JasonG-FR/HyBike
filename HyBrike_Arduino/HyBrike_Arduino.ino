0c8b224149d96036331c2f956d2921096d1de064
JasonG-FR <jason.gombert@gmail.com>
2015-12-18 04:15:10 +0100

    Ajout de la prise en charge matériel pour le frein sur l'Arduino

--- HyBrike_Arduino/HyBrike_Arduino.ino
+++ HyBrike_Arduino/HyBrike_Arduino.ino
@@ -1,5 +1,7 @@
 const int accelerateurSensor = A0;
+const int freinSensor = A1;
 long accelerateurValue = 0L;
+long freinValue = 0L;
 const int tauxActu = 1000/30;
 
 // the setup function runs once when you press reset or power the board
@@ -10,20 +12,29 @@ void setup() {
 // the loop function runs over and over again forever
 void loop() {
   accelerateurValue = 0;
+  freinValue = 0;
   for(int i = 0; i < tauxActu; i++) {
     accelerateurValue += analogRead(accelerateurSensor);
+    freinValue += analogRead(freinSensor);
     delay(1);
   }
+  
   if(accelerateurValue/tauxActu == 1022) {
     accelerateurValue = 1023;
   }
   else {
     accelerateurValue = accelerateurValue/tauxActu;
   }
+
+  if(freinValue/tauxActu == 1022) {
+    freinValue = 1023;
+  }
+  else {
+    freinValue = freinValue/tauxActu;
+  }
   
   //Variables de simulation à développer
   int batterieValue = 512;
-  int freinValue = 256;
 
   //Renvoi console sous forme csv (séparateur ;)
   Serial.print(accelerateurValue);
